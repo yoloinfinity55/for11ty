77a6144044473685fe306fee775f9362a7705545
yoloinfinity55 <yoloinfinity55@gmail.com>
2025-09-17 11:18:50 -0400

    update css path

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+
+  const pathPrefix = process.env.NODE_ENV === 'production' ? '/for11ty/' : '/';
+  
   //Passthrough copy for CSS
   eleventyConfig.addPassthroughCopy("src/css");
   return {
